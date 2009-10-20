3ffc3eda101181f25159118bd8dd075acf80d213
ancientpc <ancientpc@13e3b278-aed6-11de-ab95-49b569c737e1>
2009-10-20 16:40:49 +0000

    initial load

---
+++ trunk/main.cpp
@@ -0,0 +1,22 @@
+/**
+	\file main.cpp
+	\brief CS 371D Project - Playlist Editing
+
+	\author HanPing Ting
+	\author Will Warner
+*/
+
+#include "Node.h"
+
+#ifdef DEBUG_OUTPUT
+	#define debug(arg) cout << (arg) << endl;
+#else
+	#define debug(arg) /* arg */
+#endif
+
+int main () {
+    using namespace std;
+    using namespace dist;
+
+    return 0;
+}
