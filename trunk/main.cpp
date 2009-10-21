8b1df4bf40e92060da201d4c884b5e51e842ffb0
ancientpc <ancientpc@13e3b278-aed6-11de-ab95-49b569c737e1>
2009-10-21 22:55:09 +0000

    finished Logger class

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -19,9 +19,22 @@ int main(int argc, char* argv[]) {
     using namespace std;
     using namespace dist;
 
-	Node* A = new Node();
-
+	/*Node* A = new Node();
 	delete A;
+	*/
 
+	Logger log(true,0,"socket_fake.log");
+
+	string input;
+	while (true) {
+		cin >> input;
+		if (cin.eof())
+			break;
+		log.write(0,"input line: " + input);
+	}
+
+	log.read();
+	log.read_head(1);
+	log.read_tail(2);
     return 0;
 }
