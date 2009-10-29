b86f32c138e5d6d59a4c45caafa9055e18e7ee99
ancientpc <ancientpc@13e3b278-aed6-11de-ab95-49b569c737e1>
2009-10-29 00:04:28 +0000

    still debugging server receive handler

--- trunk/network.h
+++ trunk/network.h
@@ -1,6 +1,7 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <cstdio>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -17,6 +18,7 @@ using aux::to_str;
 using boost::asio::ip::tcp;
 
 namespace network {
+	enum { max_length = 1024 };
 
 class client :
 	//keep alive as long as something is pointing to this
@@ -43,7 +45,7 @@ public:
 
 	string send(string msg) {
 		try {
-			log->write(4,to_str("send: ") + msg);
+			log->write(4,to_str("network::client::send(): ") + msg);
 
 			tcp::resolver resolver(io); //turns io into TCP endpoint
 			tcp::resolver::query query(tcp::v4(), host, port); //constructing query
@@ -51,7 +53,7 @@ public:
 			tcp::resolver::iterator end;
 
 			//creates socket and opens connection
-			log->write(0,to_str("creating socket to ") + host + to_str(":") + port);
+			log->write(0,to_str("network::client::send() - creating socket to ") + host + to_str(":") + port);
 			tcp::socket s(io);
 
 			boost::system::error_code error = boost::asio::error::host_not_found;
@@ -63,22 +65,21 @@ public:
 			if (error)
 				throw boost::system::system_error(error);
 
-			char ack[1024];
+			char ack[max_length];
 			string reply;
 
-			log->write(3,to_str("sending:  ") + msg);
-			log->write(0,"write buffer");
+			log->write(0,"network::client::send() - write buffer");
 			boost::asio::write(s,boost::asio::buffer(msg,msg.size()));
-			log->write(0,"read  buffer");
+			log->write(0,"network::client::send() - read  buffer");
 			boost::asio::read(s,boost::asio::buffer(ack,msg.size()));
 			reply = to_str(ack).substr(0,msg.size());
-			log->write(3,to_str("response: ") + to_str(reply));
+			log->write(3,to_str("network::client::send() - response: ") + to_str(reply));
 
 			s.close();
-			log->write(4,to_str("send end"));
+			log->write(4,to_str("network::client::send() - send end"));
 			return to_str(reply);
 		} catch (exception& e) {
-			cerr << "Exception:" << e.what() << endl;
+			cerr << "network::client::send() - exception:" << e.what() << endl;
 		}
 
 		return NULL;
@@ -86,20 +87,45 @@ public:
 };
 
 class session {
+	tcp::socket socket_;
+	char data_[max_length];
+
+	logger* log;
+
 public:
-	session(boost::asio::io_service& io_service) : socket_(io_service) { }
+	session(boost::asio::io_service& io_service,logger* l) : socket_(io_service) {
+		log = l;
+		log->write(1,"network::session()");
+	}
 
 	tcp::socket& socket() { return socket_; }
 
 	void start() {
+		//read data from client
+		log->write(1,"network::session::start()");
 		socket_.async_read_some(boost::asio::buffer(data_, max_length),
 				boost::bind(&session::handle_read, this,
 				boost::asio::placeholders::error,
 				boost::asio::placeholders::bytes_transferred));
+
+	}
+
+	/*char* timestamp(char* in) {
+		string tmp = *in;
+		tmp = "[timestamp]" + tmp;
+		return tmp.c_str();
+	}*/
+	void timestamp() {
+		strcat(data_,"_timestamp_modification");
 	}
 
 	void handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
+		log->write(1,"network::session::handle_read()");
 		if (!error) {
+			//write data to client
+			//data_ = timestamp(&data_);
+			timestamp();
+
 			boost::asio::async_write(socket_,
 					boost::asio::buffer(data_, bytes_transferred),
 					boost::bind(&session::handle_write, this,
@@ -108,6 +134,7 @@ public:
 	}
 
 	void handle_write(const boost::system::error_code& error) {
+		log->write(1,"network::session::handle_write()");
 		if (!error) {
 			socket_.async_read_some(boost::asio::buffer(data_, max_length),
 					boost::bind(&session::handle_read, this,
@@ -115,11 +142,6 @@ public:
 					boost::asio::placeholders::bytes_transferred));
 		} else { delete this; }
 	}
-
-private:
-	tcp::socket socket_;
-	enum { max_length = 1024 };
-	char data_[max_length];
 };
 
 class server {
@@ -138,10 +160,10 @@ public:
 			log->write(5,"network::server()");
 			port = boost::lexical_cast<short int>(p);
 
-			node = new dist::node();
+			node = new dist::node(l);
 
 			//creates new session for use with async
-			session* new_session = new session(io_service_);
+			session* new_session = new session(io_service_, log);
 
 			//initiates async_accept operation to wait for new connections
 			acceptor_.async_accept(new_session->socket(), boost::bind(&server::handle_accept, this, new_session, boost::asio::placeholders::error));
@@ -154,9 +176,10 @@ public:
 
 	//event handler called by async_accept, services client request and then initiates next async_accept operation
 	void handle_accept(session* new_session, const boost::system::error_code& error) {
+		log->write(1,"network::server::handle_write()");
 		if (!error) {
 			new_session->start();
-			new_session = new session(io_service_);
+			new_session = new session(io_service_, log);
 			acceptor_.async_accept(new_session->socket(), boost::bind(&server::handle_accept, this, new_session, boost::asio::placeholders::error));
 		} else { delete new_session; }
 	}
